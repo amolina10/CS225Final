459c899b4d95d580bbe740ab20112e18737fbf44
amolina10 <45402846+amolina10@users.noreply.github.com>
2018-12-06 11:13:39 -0700

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -13,35 +13,15 @@ Game::Game()
 
 void Game::playGame()
 {
-    // get bets
-    bet();
-    // push off first deal
-    /*****************************************how do i know if i havent added points*/
+ 
+    bet();// get bets
     
-    if (pushed())
+    if ((p1.getPoints() == 21) && (dl.getPoints() == 21))
     {
-        cout<< "tie bet returned" << endl;
+        cout<< "tie, bet returned" << endl;
         // return bets to players
         p1.incFunds(playerBet);
         dl.incFunds(playerBet);
-        
-    }
-    // dealer busts off the deal
-    else if (busted(dl))
-    {
-        // standard winnings are 2.5x
-        p1.incFunds(playerBet * 2.5);
-        // give winnings to player and ask to deal again
-        cout << green << " Dealer Busted on Deal, You Win 2.5x bet!" << endl
-        << "Your Pot is now = " << p1.getFundsVal() << white << endl;;
-    }
-    // player busted on deal
-    else if(busted(p1))
-    {
-        p1.decFunds(playerBet);
-        // give winnings to dealer and ask to deal again
-        cout << green << " Player Busted on Deal, You lose!" << endl
-        << "Your Pot is now = " << p1.getFundsVal() << white << endl;;
     }
     // play the game
     else
@@ -50,54 +30,64 @@ void Game::playGame()
         cout << red << "Would you like to double down? Enter 'y' for yes:  " << endl;
         cin >> choice;
 
-        if (tolower(choice) == 'y') {
-            pot += playerBet;
-            p1.decFunds(playerBet);
-            doubleDown = true;
-            cout << p1.getFundsVal() << endl;
-        }
-        
-        // player hit or stay?
-        if(p1.getPoints() <21 ){
-            cout << red << "hit or stay? Enter h to hit:  " << endl;
-            cin >> choice;
-            if (tolower(choice) == 'h')
-            {
+        if (tolower(choice) == 'y') { //yes double down
+            //check if enough founds
+            if((p1.getFundsVal()- playerBet) >= p1.getFundsVal()){ 
+                p1.decFunds(playerBet);
+                cout << "New Funds:" p1.getFundsVal() << endl;
                 hit(p1, thisDeck.drawCard(), choice);
             }
+            else{
+                choice == 'n'; //manually go to not doubling down
+                break;
+            }
         }
-        else
-            cout<< "player hit over 21" <<endl;
         
-        
-         cout << "dealers turn" << endl;
-            dealerOps();
-        }
-        // dealers turn to hit or stay
-        //dealerOps();
+        if(tolower(choice) == 'n'){ //no dont double down
+            cout << red << "hit(h) or stay(s)? :  " << endl;
+            cin >> choice;
+            while (tolower(choice) == 'h') //player hits
+            {
+               hit(p1, thisDeck.drawCard(), choice);
+               if(!(busted(p1)))
+               {
+                    cout << red << "Enter h to hit again:  " << white;
+                    cin >> choice;
+               }
+               else //busted
+               {
+                   cout << cyan << "You busted" << endl;
+                   p1.decFunds(playerBet);
+                   dl.incFunds(playerBet);
+                   break;
+               }
+            }  
+            cout << "dealers turn" << endl;         // dealers turn to hit or stay
+                hit(dl, thisDeck.drawCard(), choice);
+        }      
+
         if (pushed())
         {
             cout << red << "Push, monies returned to players" << white << endl;
-            pot = 0;
             p1.incFunds(playerBet);
             dl.incFunds(playerBet);
         }
-        else if (dl.getPoints() > p1.getPoints())
+       if ((dl.getPoints() > p1.getPoints()) && (dl.getPoints() <21)))
         {
             cout << grey << "YOU LOST!" << white << endl;
         }
-        else
+       if ((p1.getPoints() > dl.getPoints()) && (p1.getPoints() <21)))
         {
             cout << green << "YOU WON!" << white << endl;
+            p1.incFunds(playerBet*2);
+            "Your Pot is now = " << p1.getFundsVal() << white << endl;;
         }
-    
 }
 
 bool Game::pushed()
 {
-    if ((p1.getPoints() == 21) && (dl.getPoints() == 21))
+    if (p1.getPoints() == dl.getPoints())
     {
-        cout << red << " Push!" << white << endl;
         return true;
     }
     else return false;
@@ -119,38 +109,34 @@ void Game::bet()
     if (playerBet <= p1.getFundsVal())
     {
         p1.decFunds(playerBet);
-        pot += 2 * playerBet;
         dl.decFunds(playerBet);
         cout << red << "Your funds are now:  " << p1.getFundsVal() << endl
         << "House Funds are now:  " << dl.getFundsVal() << white << endl;
     }
 }
 
-void Game::dealerOps()
-{
-    //hit if dealer has less than 17 points
-    while (dl.getPoints() < 17) {
-        hit(dl, thisDeck.drawCard(), 'h');
-        }
-}
-
 void Game::hit(Player anyPlayer, Card someCard, char choice) {
-
     if (anyPlayer.getPlayer() == playerType::dealer)
     {
-        //p1.decFunds(playerBet);
-       // cout << red << p1.getFundsVal() << white << endl;
-        anyPlayer.addCard(someCard);
-        anyPlayer.getPoints();
+        //manually hit if dealer has less than 17 points
+        while (dl.getPoints() < 17) {
+            dl.addCard(someCard);
+            if(!busted(dl))
+            {
+                
+            }
+            else
+            {
+                cout<< "Dealer bust" <<endl;
+                p1.incFunds(playerBet*2);
+                cout<< cyan<< "New Player Funds:" << p1.getFundsVal() << endl;
+                break;
+            }
+        }
     }
     else {
-        while (tolower(choice) == 'h')
-        {
-                cout << red << "Enter h to hit again:  " << white;
-                cin >> choice;
-                p1.decFunds(playerBet);
-                cout << red << p1.getFundsVal() << white << endl;
-                anyPlayer.addCard(someCard);
-        }
+           p1.addCard(someCard);
+        //cout << cyan << "Player drew: " << p1<< white << endl;
+        cout<< cyan<< "New Player Total:" << p1.getPoints() << endl;
     }
 }
